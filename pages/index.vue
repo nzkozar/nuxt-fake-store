010b8c4e8861067faeeb65662badaeceee7480cb
Anže Kožar <nzkozar@gmail.com>
2024-06-09 19:18:06 +0200

    Added categories and category details. Refreshed product details page.

--- pages/index.vue
+++ pages/index.vue
@@ -6,17 +6,26 @@ definePageMeta({
 });
 
 useHead({
-  title: "Products",
+  title: "FakeStore | Home",
   meta: [
-    {name: "description", content: "Description of Browse books from Browse books." }
+    {name: "description", content: "Fake products for all!" }
   ]
 });
+
+const {data: categories} = await useFetch('https://fakestoreapi.com/products/categories/');
+
 </script>
 
 <template>
   <HorizontalProductList :title="$t('home.featured_products')"
                          linkMore="/products/featured"
                          listDataUrl="https://fakestoreapi.com/products?limit=10"/>
+
+  <HorizontalProductList v-for="category in categories"
+                         :title="category"
+                         :linkMore="'/category/'+category"
+                         :listDataUrl="'https://fakestoreapi.com/products/category/'+category+'?limit=10'"/>
+
 </template>
 
 <style scoped>
---
+++ pages/products/featured.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ProductCard from "~/components/ProductCard.vue";
+
+definePageMeta({
+  layout: 'landing'
+});
+
+useHead({
+  title: "Featured Products",
+  meta: [
+    {name: "description", content: "FakeStore product categories" }
+  ]
+});
+
+const {data: products} = await useFetch('https://fakestoreapi.com/products?limit=20');
+
+</script>
+
+<template>
+  <h1>{{$t('home.featured_products')}}</h1>
+  <div class="row">
+    <ProductCard v-for="product in products" :product="product" class="col-6 col-md-4 col-lg-2 mb-3 mx-auto"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
